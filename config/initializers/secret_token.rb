b1ec9daecc04599b59318379053caf226a8eec32
rcn-neo-yang <rcn.neo.yang@gmail.com>
2014-04-28 16:30:45 +0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreeApp::Application.config.secret_key_base = '46d59d1a1b82019260aa0de7433f1d398ecafd2be3bdc039b839ff0273e000d9ea9660d578f85274c044d4ec94d6d3b6a828e823f7d5bab732a052f4ae51104c'
